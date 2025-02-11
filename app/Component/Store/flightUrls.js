7e90d9321da18af2e63037eca68acbc338ce3aac
vinitanwar <125426162+vinitanwar@users.noreply.github.com>
2025-02-11 14:47:01 +0530

    add-dones

---
+++ app/Component/Store/flightUrls.js
@@ -0,0 +1,5 @@
+
+export const localurl = "https://himanshu.futuretouch.org/api"
+export const imgurl = "https://himanshu.futuretouch.org/api"
+
+// export const localurl = "https://admin.apkatrip.com/api/v1"
---
+++ app/Component/common/index.js
@@ -0,0 +1,5 @@
+export const apilink = 'https://himanshu.futuretouch.org/api/v1'
+//https://admin.gursimran.online
+export const imgurl = 'https://apkatripbucket.s3.ap-south-1.amazonaws.com'
+export const development= "roduction";
+//http://localhost:8000
